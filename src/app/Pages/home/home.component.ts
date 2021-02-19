133fba962ad151815eb2e66150c38342d140d943
Juan Diego Tovaria <juken.555@hotmail.com>
2021-02-19 09:32:22 -0500

    Side bar menu, mouse over events, created

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  mini = true;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toggleSidebar() {
+    if (this.mini) {
+      document.getElementById("mySidebar").style.width = "18vw";
+      document.getElementById("main").style.marginLeft = "18vw";
+      document.getElementById("main").style.width = "82vw";
+      this.mini = false;
+    } else {
+      document.getElementById("mySidebar").style.width = "6vw";
+      document.getElementById("main").style.marginLeft = "6vw";
+      this.mini = true;
+    }
+  }
+
 
 }
